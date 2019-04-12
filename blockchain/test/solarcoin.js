15317e893eeb536d1d8620f1abed14961483dc15
Ruaidhri Hogan <ruaidhri.hogan@gmail.com>
2019-04-12 18:02:41 +0200

    Updated tests

--- blockchain/test/solarcoin.js
+++ blockchain/test/solarcoin.js
@@ -4,14 +4,12 @@ contract('SolarCoin', async accounts => {
 
     it("should allow a report to be announced", async () => {
         let instance = await SolarCoin.deployed();
-        await instance.register.call();
         let balance = await instance.report.call(100, 100);
         assert.equal(balance.toNumber(), 100);
     });
 
     it("should return balance correctly", async () => {
         let instance = await SolarCoin.deployed();
-        await instance.register.call();
         let balance = await instance.getMyBalance.call();
         assert.equal(balance.toNumber(), 0);
     });
