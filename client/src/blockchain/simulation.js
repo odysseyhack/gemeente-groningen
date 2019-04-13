85d0249cd10da5c51979648cc722ead0c1099135
Ruaidhri Hogan <ruaidhri.hogan@gmail.com>
2019-04-13 16:50:37 +0200

    added randomization

--- client/src/blockchain/simulation.js
+++ client/src/blockchain/simulation.js
@@ -6,15 +6,25 @@ const data = JSON.parse(fs.readFileSync('production.json'));
 
 console.log(data);
 
+const rand = (x) => {
+    const y = x / 10;
+    const r = Math.random()*y;
+    const a = Math.round(r);
+    if (a === 0) {
+        return x + r;
+    }
+    return x - r;
+};
+
 const loop = (accounts, instance, callback) => {
     setTimeout(callback, 96*TICK*1000);
     for(let i = 0; i < 96; i += 1) {
         setTimeout(() => {
             accounts.forEach((account) => {
-                instance.report(parseInt(data[i], 10), 100, {from: account});
+                instance.report(parseInt(rand(data[i]), 10), 100, {from: account});
             });
             instance.getMyBalance.call().then((res) => {
-                console.log(`Generated: ${data[i]} Consumed: 100, Earned: ${res.toNumber()}`);
+                console.log(`Generated: ~${Math.floor(rand(data[i]))} Consumed: ~100, Earned: ${res.toNumber()}`);
             })
         }, (i + 1)*TICK*1000);
     }
